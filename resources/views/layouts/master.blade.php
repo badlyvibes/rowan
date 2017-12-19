b13805d411a687b87bc3a0b8298faf435713632c
Simon Bennett <simon.bennett@influentialsoftware.com>
2017-12-19 09:02:16 +0000

    Changed word order in title to move tree name to front of phrase

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 
     <head>
 
-        <title>Rowan: A Database of Common Tree Names | {{$context}}</title>
+        <title>{{$context}} | Rowan: A Database of Common Tree Names</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,14 +17,13 @@
 
     <div class="container-fluid">
 
-        <a href="/trees"><img style="height: 100px; padding-top: 4px" src="{{ asset('images/sorbus_aucuparia.svg') }}" alt="Index"></a>
+        <a href="/trees"><img style="height: 80px; padding-top: 4px" src="{{ asset('images/sorbus_aucuparia.svg') }}" alt="Index"></a>
 
         <hr style="border: 1px solid #b5b5b5">
 
         @yield('content')
 
     </div>
-
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
