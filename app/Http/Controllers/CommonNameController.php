b0f82e605c90a6e68dd1119fcd5b6d716ffedac7
Simon Bennett <simon.bennett@influentialsoftware.com>
2017-12-21 17:56:44 +0000

    Added controller for common names

---
+++ app/Http/Controllers/CommonNameController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CommonName;
+use App\Tree;
+
+use Request;
+
+class CommonNameController extends Controller
+{
+    public function add()
+    {
+        $input = Request::all();
+
+        $commonName = new CommonName();
+
+        $commonName->name = $input['name'];
+        $commonName->tree_id = $input['tree_id'];
+        $commonName->language_id = $input['language_id'];
+        $commonName->user_id = $input['user_id'];
+
+        $commonName->save();
+
+        $tree = Tree::id($commonName->tree_id)[0];
+
+        $genus = $tree->genus;
+        $species = $tree->species;
+
+        return redirect()->action('TreesController@showSpecies', compact('genus', 'species'));
+    }
+}
