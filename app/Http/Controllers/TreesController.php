d81c240e1951ecb172d29845a3d607bf81170317
Simon Bennett <simon.bennett@influentialsoftware.com>
2017-12-21 17:56:20 +0000

    Added search

--- app/Http/Controllers/TreesController.php
+++ app/Http/Controllers/TreesController.php
@@ -3,34 +3,62 @@
 namespace App\Http\Controllers;
 
 use App\Tree;
+use App\Language;
+use Request;
 
 class TreesController extends Controller
 {
 
     public function index()
     {
-        $trees = Tree::name('florid');
-        $context = 'Index';
-        return view('layouts.index', compact('trees', 'context'));
+        $trees = [];
+        for ($i = 0; $i <= 4; $i++) {
+            $trees[] = Tree::id(rand(1, 60000))[0];
+        }
+        $context = ['type' => 'index', 'title' => 'Index'];
+        return $this->returnLayout($trees, $context);
+    }
+
+    public function postSearch()
+    {
+        return redirect()->action('TreesController@getSearch', ['search' => Request::all()['search']]);
+    }
+
+    public function getSearch($search)
+    {
+        $context = ['type' => 'search', 'term' => $search, 'title' => 'Search: ' . $search];
+        return $this->returnLayout(Tree::search($search)->paginate(10), $context);
     }
 
     public function show(Tree $tree)
     {
-        $context = $tree->name . ' | ID ' . $tree->id;
-        return view('layouts.show', compact('tree', 'context'));
+        $context = ['type' => 'show', 'title' => $tree->name  . ' | ID ' . $tree->id];
+        $languages = Language::selectList();
+        return view('layouts.show', compact('tree', 'context', 'languages'));
     }
 
     public function showSpecies($genus, $species)
     {
         $tree = Tree::species($genus, $species);
-        $context = $tree->name;
-        return view('layouts.show', compact('tree', 'context'));
+        $context = ['type' => 'species', 'title' => 'Species: ' . $tree->name];
+        $languages = Language::selectList();
+        return view('layouts.show', compact('tree', 'context', 'languages'));
+    }
+
+    public function showSpeciesById(Tree $tree)
+    {
+        return $this->showSpecies($tree->genus, $tree->species);
     }
 
     public function showGenus($val)
     {
-        $trees = Tree::genus($val);
-        $context = ucfirst($val);
-        return view('layouts.index', compact('trees', 'context'));
+        $context = ['type' => 'genus', 'title' => 'Genus: ' . ucfirst($val)];
+        return $this->returnLayout(Tree::genus($val), $context);
+    }
+
+    private function returnLayout($trees, $context)
+    {
+        $languages = Language::selectList();
+        return view('layouts.index', compact('trees', 'context', 'languages'));
     }
 }
