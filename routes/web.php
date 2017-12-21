3b0c68d47fc9b579c9d0c26edcc2e2fb265db3da
Simon Bennett <simon.bennett@influentialsoftware.com>
2017-12-21 17:54:28 +0000

    Added more routes for POSTing

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/trees', 'TreesController@index');
+Route::post('/add_common_name', 'CommonNameController@add');
+Route::post('/search', 'TreesController@postSearch');
+Route::get('/search/{search}', 'TreesController@getSearch');
 Route::get('/tree/{tree}', 'TreesController@show');
 Route::get('/genus/{genus}', 'TreesController@showGenus');
 Route::get('/genus/{genus}/species/{species}', 'TreesController@showSpecies');
